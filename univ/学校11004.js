bf0120169e34730073f54d0fffa6700202c860a0
Rocinantie <44108968+Rocinantie@users.noreply.github.com>
2018-10-15 22:31:24 +0800

    Create 学校11004.js

---
+++ univ/学校11004.js
@@ -0,0 +1,13 @@
+var univ_list = {
+    school: "武汉理工",                        // 学校名称
+    id: "11004",                               // 学校 ID
+    link: [ {
+        name: "武汉理工大学教务处",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://jwc.whut.edu.cn/"   // 链接地址
+    }, {
+        ...
+    }, {
+        name: "武汉理工大学教务管理系统",                        // 链接数量小于或等于7个
+        url: "http://sso.jwc.whut.edu.cn/Certification/toLogin.do#"
+    } ]
+};
